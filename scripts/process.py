5a9bb21907de42c98d4f94f9b2cd7dae92446f25
Gregg Lind <gregg.lind@gmail.com>
2015-05-22 09:36:03 -0500

    moved to scripts

--- scripts/process.py
+++ scripts/process.py
@@ -4,10 +4,14 @@ from collections import defaultdict, Counter
 from itertools import permutations
 
 out = defaultdict(lambda: Counter())
+
+# precache all the ranks
+ranks = defaultdict(lambda: dict())
+
 for line in fileinput.input(sys.argv[1:]):
     files = line.split()
     changeset = files.pop(0)
-    if len(files) > 20: # somewhat arbitrary
+    if len(files) > 100: # somewhat arbitrary
         continue
 
     for f in files:
@@ -21,10 +25,20 @@ for line in fileinput.input(sys.argv[1:]):
         out[f][f] += 1  # solo pairing!
 
 
+# loop once to fill ranks.
+for (p1) in (out):
+    for (ii,(p2,c)) in enumerate(out[p1].most_common()):
+        ranks[p1][p2] = ii  #
+
 for (p1) in (out):
     for (p2,c) in (out[p1].iteritems()):
-        print "\t".join([p1,p2,str(c), "*" * int(p1 == p2)])
+        r1 = ranks[p1][p2]
+        r2 = ranks[p2][p1]
+        print "\t".join([p1,p2,str(c), ['-', "self"][int(p1 == p2)], str(r1), str(r2), ['-', "<3"][r1==r2==1] ])
 
 
+
+# ideas.... ranks....
+# 1 vs #1
 
 
